adc49e95ab618f4148af58a7fe3de4335dc4a8ad
Greg Wientjes <gwientjes@gmail.com>
2017-02-18 14:44:31 -0800

    comment out code we don't need

--- supost_watson.py
+++ supost_watson.py
@@ -42,23 +42,26 @@ with open(join(dirname(__file__), '../resources/car.jpg'), 'rb') as image_file:
 
 print(json.dumps(visual_recognition.classify(images_url=test_url), indent=2))
 
-print(
-    json.dumps(visual_recognition.detect_faces(images_url=test_url), indent=2))
+# face recognition - we don't need it
+# print(
+#     json.dumps(visual_recognition.detect_faces(images_url=test_url), indent=2))
 
 # print(json.dumps(visual_recognition.delete_classifier(classifier_id='YOUR
 # CLASSIFIER ID'), indent=2))
 
 print(json.dumps(visual_recognition.list_classifiers(), indent=2))
 
-with open(join(dirname(__file__), '../resources/text.png'), 'rb')\
-        as image_file:
-    print(json.dumps(visual_recognition.recognize_text(images_file=image_file),
-                     indent=2))
+# text recognition - we don't need it
+# with open(join(dirname(__file__), '../resources/text.png'), 'rb')\
+#         as image_file:
+#     print(json.dumps(visual_recognition.recognize_text(images_file=image_file),
+#                      indent=2))
 
-with open(join(dirname(__file__), '../resources/face.jpg'), 'rb')\
-        as image_file:
-    print(json.dumps(visual_recognition.detect_faces(images_file=image_file),
-                     indent=2))
+# face recognition - we don't need it
+# with open(join(dirname(__file__), '../resources/face.jpg'), 'rb')\
+#         as image_file:
+#     print(json.dumps(visual_recognition.detect_faces(images_file=image_file),
+#                      indent=2))
 
 # with open(join(dirname(__file__), '../resources/face.jpg'), 'rb') as \
 #     image_file:
