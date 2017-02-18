edfc17ff0a273a27f9f03b399e671aff77fc1ad8
Greg Wientjes <gwientjes@gmail.com>
2017-02-18 14:08:45 -0800

    modified to new csv file format

--- get_images.py
+++ get_images.py
@@ -3,12 +3,13 @@ import urllib
 
 def main():
 
-	with open('negative_2.csv', 'rb') as f:
+	with open('../bikes_subcat_4.csv', 'rb') as f:
 		reader = csv.reader(f)
 		ids = list(reader)
 		
 		for x in range(1,100):
-			url = "http://supost-prod.s3.amazonaws.com/posts/" + ids[x][0][:-1] + "/post_" + ids[x][0]
-			urllib.urlretrieve(url, "negative2/" + ids[x][0] + ".jpg")
+			# url = "http://supost-prod.s3.amazonaws.com/posts/" + ids[x][0][:-1] + "/post_" + ids[x][0]
+			url = ids[x][0]
+			urllib.urlretrieve(url, "../bikes/" + ids[x][1] + ".jpg")
 
 main()
