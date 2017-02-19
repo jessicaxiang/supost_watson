07ca3fd44846c0049b6437f75ad368d1cb7e9c55
Jessica Xiang <jessicaxiang1997@gmail.com>
2017-02-18 21:15:01 -0800

    small layout fixes

--- supost_watson.py
+++ supost_watson.py
@@ -29,6 +29,7 @@ def get_classifier_id():
 	print(json.dumps(visual_recognition.list_classifiers(), indent=2))
 
 def get_classification(image):
+	#print(json.dumps(visual_recognition.classify(images_url=image, threshold=0.0,classifier_ids=[CLASSIFIER_ID]), indent=2))
 	result = visual_recognition.classify(images_url=image, threshold=0.0,classifier_ids=[CLASSIFIER_ID])
 	bike_score = result['images'][0]['classifiers'][0]['classes'][0]['score']
 	car_score = result['images'][0]['classifiers'][0]['classes'][1]['score']
@@ -49,7 +50,7 @@ def get_tags(image):
 	return tags
 
 # def main():
-# 	get_tags(bike_test_url)
+# 	get_classification("http://theradavist.com/wp-content/uploads/2016/04/bicycle-crumbs-grimes-01.jpg")
 
 # main()
 
